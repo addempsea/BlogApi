9bfe3433ee09b3ac5bda7a7b2d8a136499280c7e
addempsea <babatundeademola4@gmail.com>
2019-11-28 20:19:03 +0100

    first commit

---
+++ models/blog.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    content: {type: String, required: true},
+    img: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('Post', postSchema);
