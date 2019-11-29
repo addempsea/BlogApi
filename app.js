0537fbee4af0367a2da527f87499f8389a7f7341
Tejiri Amami <54507650+Amami27@users.noreply.github.com>
2019-11-29 07:33:20 +0100

    Controllers (#2)
    * The controller and some addition
    
    * refactored

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var dotenv = require('dotenv').config();
 var logger = require('morgan');
+
+var port = 8000
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+mongoose.connect(process.env.DATABASE_URL);
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const controllerx = require('../controllers/user');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.status(200).json({
+        message: 'Welcome to our  Blog API'
+    });
 });
+
+router.post('/login', controllerx.login);
+router.post('/signup', controllerx.signup);
+router.get('/total', controllerx.total);
+router.put('/updateuser/:id', controllerx.updateUser);
 
 module.exports = router;
